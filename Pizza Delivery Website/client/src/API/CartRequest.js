206f1db2b86e58bf339abfbe898c2d5915a37799
Saif-Ustad <121380262+Saif-Ustad@users.noreply.github.com>
2023-11-15 02:12:55 +0530

    Update CartRequest.js

--- Pizza Delivery Website/client/src/API/CartRequest.js
+++ Pizza Delivery Website/client/src/API/CartRequest.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({baseURL: "http://localhost:5000"});
+const API = axios.create({baseURL: "https://pizza-delivery-website-0lyf.onrender.com"});
 
 export const addToCart = (product, userId) => API.put(`/cart/${userId}/add`, product);
 export const removeFromCart = (productId, userId) => API.put(`/cart/${userId}/remove`, productId);
