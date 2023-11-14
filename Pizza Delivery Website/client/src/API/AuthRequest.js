8e97d9e918b790c16db78a5f22c26f8c6856f868
Saif-Ustad <121380262+Saif-Ustad@users.noreply.github.com>
2023-11-15 02:36:14 +0530

    Update AuthRequest.js

--- Pizza Delivery Website/client/src/API/AuthRequest.js
+++ Pizza Delivery Website/client/src/API/AuthRequest.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({baseURL: "https://pizza-delivery-website-0lyf.onrender.com"});
+const API = axios.create({baseURL: "http://localhost:5000"});
 
 export const login = (FormData) => API.post("/auth/login", FormData);
 export const register = (FormData) => API.post("/auth/register", FormData);
