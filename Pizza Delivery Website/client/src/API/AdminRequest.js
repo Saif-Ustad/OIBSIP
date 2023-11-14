9b23b50afc65453be5875f02d841d29e6be147b3
Saif-Ustad <121380262+Saif-Ustad@users.noreply.github.com>
2023-11-15 02:35:55 +0530

    Update AdminRequest.js

--- Pizza Delivery Website/client/src/API/AdminRequest.js
+++ Pizza Delivery Website/client/src/API/AdminRequest.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({baseURL: "https://pizza-delivery-website-0lyf.onrender.com"});
+const API = axios.create({baseURL: "http://localhost:5000"});
 
 export const getAllAdminOrders = () => API.get("/admin");
 export const updateOrderStatus = (orderId, userId, status) => API.put("/admin/updateorder", {orderId ,userId, status});
