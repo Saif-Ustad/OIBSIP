a45d600ec5491b174c3d8b499ff3df1b197f7015
Saif-Ustad <121380262+Saif-Ustad@users.noreply.github.com>
2023-11-15 02:37:16 +0530

    Update ProductRequest.js

--- Pizza Delivery Website/client/src/API/ProductRequest.js
+++ Pizza Delivery Website/client/src/API/ProductRequest.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({baseURL: "https://pizza-delivery-website-0lyf.onrender.com"});
+const API = axios.create({baseURL: "http://localhost:5000"});
 
 export const getAllProducts = () => API.get("/products");
 export const getSingleProduct = (ProductId) => API.get(`/products/${ProductId}`);
